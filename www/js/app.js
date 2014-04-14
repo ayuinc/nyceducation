ee5425810bfe6d5da4008474c3ba0ee9e7773dbd
Ricardo Diaz <ricardotk002@gmail.com>
2014-04-14 16:55:16 -0500

    updates

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,33 @@
-var app = angular.module("nyce", ["ngRoute"]);
+var app = angular.module("nyce", ["ngRoute", "ngAnimate", "ngTouch", "nyce.memoryServices"]);
 
 app.config(function($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "templates/main.html",
-        controller: "mainController"
-    })
-    .otherwise({ redirect_to: "/" });
+        controller: "MainController"
+    });
+
+    $routeProvider.when("/school/:schoolId", {
+        templateUrl: "templates/SchoolDetail.html",
+        controller: "SchoolDetailController"
+    });
+
+    $routeProvider.otherwise({ redirect_to: "/" });
 });
 
-app.controller("mainController", function($scope) {
+app.controller("MainController", ['$scope', '$rootScope', '$window', '$location', function($scope, $rootScope, $window, $location) {
     $scope.header = { title: "Welcome"};
-});
+    $scope.slide = '';
+    $rootScope.back = function() {
+        $scope.slide = 'slide-right';
+        $window.history.back();
+    };
+
+    $rootScope.go = function(path) {
+        $scope.slide = 'slide-left';
+        $location.url(path);
+    };
+}]);
+
+app.controller('SchoolDetailController', ['$scope', '$routeParams', 'School', function ($scope, $routeParams, School) {
+    $scope.school = School.get({schoolId: $routeParams.schoolId});
+}]);
