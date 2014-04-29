7ceb57296f2a958e061ab22807bbae29471fbf1b
Ricardo Diaz <ricardotk002@gmail.com>
2014-04-29 12:05:13 -0500

    autocomplete example

--- www/js/app.js
+++ www/js/app.js
@@ -69,6 +69,22 @@ app.controller("MainController", ['$scope', '$rootScope', '$window', '$location'
         console.log('path ' + path);
         $location.url(path);
     };
+
+    $scope.movies = ["Lord of the Rings",
+                    "Drive",
+                    "Science of Sleep",
+                    "Back to the Future",
+                    "Oldboy"];
+
+    // gives another movie array on change
+    $scope.updateMovies = function(typed){
+        // MovieRetriever could be some service returning a promise
+        $scope.newmovies = MovieRetriever.getmovies(typed);
+        $scope.newmovies.then(function(data){
+          $scope.movies = data;
+        });
+
+    }
 }]);
 
 /*app.controller('SchoolController', ['$scope', '$routeParams', 'School', function ($scope, $routeParams, School) {
@@ -106,3 +122,30 @@ myevents.controller("EventsController", function ($scope, $http) {
         $scope.events = data;
     });
 });*/
+
+app.factory('MovieRetriever', function($http, $q, $timeout){
+  var MovieRetriever = new Object();
+
+  MovieRetriever.getmovies = function(i) {
+    var moviedata = $q.defer();
+    var movies;
+
+    var someMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel"];
+
+    var moreMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel", "The Way Way Back", "Before Midnight", "Only God Forgives", "I Give It a Year", "The Heat", "Pacific Rim", "Pacific Rim", "Kevin Hart: Let Me Explain", "A Hijacking", "Maniac", "After Earth", "The Purge", "Much Ado About Nothing", "Europa Report", "Stuck in Love", "We Steal Secrets: The Story Of Wikileaks", "The Croods", "This Is the End", "The Frozen Ground", "Turbo", "Blackfish", "Frances Ha", "Prince Avalanche", "The Attack", "Grown Ups 2", "White House Down", "Lovelace", "Girl Most Likely", "Parkland", "Passion", "Monsters University", "R.I.P.D.", "Byzantium", "The Conjuring", "The Internship"]
+    
+    console.log('i ----> ' + i.indexOf('T'));
+    if(i && i.indexOf('T')!=-1)
+      movies=moreMovies;
+    else
+      movies=moreMovies;
+
+    $timeout(function(){
+      moviedata.resolve(movies);
+    },1000);
+
+    return moviedata.promise
+  }
+
+  return MovieRetriever;
+});
